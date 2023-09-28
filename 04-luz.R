c23c7086fd329b764e0a801535b0c70355f52be3
Muhammad Khurram Shabbir <105027455+ChKhurramShabbir@users.noreply.github.com>
2023-09-28 20:15:29 +0500

    Create 04-luz.R

---
+++ 04-luz.R
@@ -0,0 +1,31 @@
+library(torch)
+library(luz)
+library(dplyr)
+
+x <- mtcars %>% select(-mpg) %>% scale()
+y <- scale(mtcars$mpg)
+
+lr <- 0.001
+
+x_t <- torch_tensor(x)
+y_t <- torch_tensor(y)
+
+fitted <- nn_linear %>% 
+  setup(
+    optimizer = optim_sgd,
+    loss = function(y_hat, y) {
+      torch_mean((y$view(c(-1, 1)) - y_hat)^2)
+    }
+  ) %>% 
+  set_opt_hparams(lr = 0.001) %>% 
+  set_hparams(in_features = 10, out_features = 1) %>% 
+  fit(
+    list(x_t, y_t),
+    epochs = 100,
+    dataloader_options = list(batch_size = 32)
+  )
+
+luz_save(fitted, "model.luz")
+
+predict(fitted, x_t)
+plot(fitted)
